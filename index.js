bdbf26ec4af4469a11e4c5e05740c2fb74741329
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-03-15 19:03:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 /* Your code goes here */
+var customerName = "bob"
+
+const leastFavoriteCustomer ="rea"
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+  return customerName;
+}
+
+function setBestCustomer(){
+  bestCustomer = "not bob"
+  return bestCustomer;
+}
+
+function overWriteBestCustomer(){
+  bestCustomer = "maybe bob"
+  return bestCustomer
+}
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = "red"
+  return leastFavoriteCustomer
+}
+
+function attemptTwoFavoriteCustomers(){
+  let favoriteCustomer = "redi"
+  let favoriteCustomer = "trt"
+}
